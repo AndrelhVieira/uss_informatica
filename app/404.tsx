55988aabf6b8c2c8257493a2b24913358ff18c45
Andre Vieira <andreluizhillerv@gmail.com>
2024-09-03 18:54:33 -0300

    feat: add 404 page

---
+++ app/404.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Custom404 = () => {
+  return (
+    <section>
+      <h1 className="text-center">404 - Página não encontrada</h1>
+    </section>
+  );
+};
+
+export default Custom404;
